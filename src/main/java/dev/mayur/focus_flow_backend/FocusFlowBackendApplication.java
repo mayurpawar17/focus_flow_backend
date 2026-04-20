50e073c796b714f36eb5ab18f6c73f74883e9a7f
mayurpawar17 <mayurpawar4908@gmail.com>
2026-04-21 00:13:40 +0530

    project initilized

---
+++ src/main/java/dev/mayur/focus_flow_backend/FocusFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.mayur.focus_flow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusFlowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mayur/focus_flow_backend/FocusFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mayur.focus_flow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusFlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
